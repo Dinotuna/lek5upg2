287d1b219bf64d89260c6838aa908013d1a4510b
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2023-10-02 08:50:30 +0200

    klar lek 5upg2

--- src/App.java
+++ src/App.java
@@ -9,8 +9,27 @@ public class App {
         tangentbordfält[2] = tangentbord.nextInt();
         tangentbordfält[3] = tangentbord.nextInt();
         System.out.println(tangentbordfält[0]+" "+tangentbordfält[1]+" "+ tangentbordfält[2]+" "+tangentbordfält[3]);
+
         int tal1 = tangentbordfält[0];
-        System.out.println(tangentbordfält[0]+" "+ tangentbordfält[3]);
+         tangentbordfält[0] = tangentbordfält[3];
+         tangentbordfält[3] = tal1;
+         int tal2 = tangentbordfält[1];
+         tangentbordfält[1] = tangentbordfält[2];
+         tangentbordfält[2] = tal2;
+         System.out.println(tangentbordfält[0]+" "+ tangentbordfält[1]+" "+ tangentbordfält[2]+" "+tangentbordfält[3]);
+
+
+         String hej = "Hej";
+         String nej = "Nej";
+         String dig = "Dig";
+         System.out.println("Gissa ett av 3 ord");
+         String skrivin = tangentbord.nextLine();
+         skrivin = tangentbord.nextLine();
+         System.out.println(skrivin.equals(hej));
+         System.out.println(skrivin.equals(nej));
+         System.out.println(skrivin.equals(dig));
+         
+         
 
         }
     }
